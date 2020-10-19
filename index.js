22118061e6cc7cc8610ad50289b7bf233fa283e4
Luke L <laroccol@msoe.edu>
2020-10-19 12:15:06 -0500

    real value

--- index.js
+++ index.js
@@ -501,7 +501,7 @@ async function EndCycle(channel) {
                 for (const player of standings) {
                     var points = POINTS_DISTRIBUTION[count];
                     await AddPointsToPlayer(player, points);
-                    embed.addField(`${count + 1}. ${player.displayName}`, `${points}`);
+                    embed.addField(`${count + 1}. ${player.displayName} - ${points} points`, `${parseChallengeValue(player.times[challenge.mapName], challenge.mapName)}`);
                     if (count >= 2) {
                         break;
                     }
